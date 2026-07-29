96c96cfb6ffd805ced808903c0b5b68b25e14078
Osama Masri <osamamasri712@gmail.com>
2026-07-29 03:11:35 +0300

    Create config.py

---
+++ university-hsm-auth/agent /config.py
@@ -0,0 +1,13 @@
+PKCS11_MODULE_PATH = r"C:\Program Files\OpenSC Project\OpenSC\pkcs11\opensc-pkcs11.dll"
+
+TOKEN_LABEL = "Pico-HSM"
+
+KEY_LABEL = "university-admin-auth-key"
+KEY_ID = b"\x01"
+
+EC_CURVE = "secp256r1"
+
+AGENT_HOST = "127.0.0.1"
+AGENT_PORT = 5050
+
+ALLOWED_ORIGIN = "http://127.0.0.1:5000"
